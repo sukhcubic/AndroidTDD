e77b5cbc8b68c3005c9f3a443458d66d37b1795e
sukhjinderji.singh@gmail.com <sukhjinder.singh@nextbus.com>
2019-07-09 22:13:31 -0500

    Ui components

--- app/src/main/java/com/example/notetaking/di/ActivityBuilderModule.java
+++ app/src/main/java/com/example/notetaking/di/ActivityBuilderModule.java
@@ -1,5 +1,6 @@
 package com.example.notetaking.di;
 
+import com.example.notetaking.ui.notes.NoteActivity;
 import com.example.notetaking.ui.noteslist.NotesListActivity;
 
 import dagger.Module;
@@ -8,5 +9,8 @@ import dagger.android.ContributesAndroidInjector;
 @Module
 public abstract class ActivityBuilderModule {
     @ContributesAndroidInjector
-    abstract NotesListActivity notesActivity();
+    abstract NotesListActivity contributeNotesListActivity();
+
+    @ContributesAndroidInjector
+    abstract NoteActivity contributeNotesActivity();
 }
--- app/src/main/java/com/example/notetaking/ui/noteslist/NotesListActivity.java
+++ app/src/main/java/com/example/notetaking/ui/noteslist/NotesListActivity.java
@@ -20,7 +20,7 @@ public class NotesListActivity extends DaggerAppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_note);
 
         Log.d(TAG, "onCreate: " + noteRepository);
 
